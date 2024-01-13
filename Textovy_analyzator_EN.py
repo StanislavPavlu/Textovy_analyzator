a03a614371e0b23ee2439ecece3c318596c6c39a
StanislavPavlu <standic@seznam.cz>
2024-01-13 23:27:30 +0100

    Čáry jako proměnné

--- Textovy_analyzator_EN.py
+++ Textovy_analyzator_EN.py
@@ -43,33 +43,38 @@ uzivatele = {
     "liz": "pass123"
 }
 
+caradvoj_kratka = 35*"="
+carajedno_kratka = 35*"-"
+caradvoj_dlouha = 50*"="
+carajedno_dlouha = 50*"-"
+
 # Přihlášení uživatele - zadání údajů:
-print(35*"=")
+print(caradvoj_kratka)
 print("TEXT ANALYZER")
-print(35*"=")
+print(caradvoj_kratka)
 jmeno = input("Enter your username:\n")
-print(35*"-")
+print(carajedno_kratka)
 heslo = input("Enter your password:\n")
-print(50*"=")
+print(caradvoj_dlouha)
 
 # Přihlášení uživatele - vyhodnocení zadaných údajů:
 if uzivatele.get(jmeno) == heslo:
     print(f"Welcome to the TEXT ANALYZER application, {jmeno}.")
 else:
     print("Incorrect user name or password, the program will be terminated.")
-    print(50*"=")
+    print(caradvoj_dlouha)
     exit()
 
 # Výběr jednoho z textů:
 pripustne_vstupy = ("1", "2", "3")
 print("Select one of the texts to analyze and enter its number:")
-print(50*"-")
+print(carajedno_dlouha)
 print(pripustne_vstupy[0] + "\n\n" + TEXTS[0])
-print(50*"-")
+print(carajedno_dlouha)
 print(pripustne_vstupy[1] + "\n\n" + TEXTS[1])
-print(50*"-")
+print(carajedno_dlouha)
 print(pripustne_vstupy[2] + "\n\n" + TEXTS[2])
-print(50*"-")
+print(carajedno_dlouha)
 
 vybrany_text = input()
 if vybrany_text == pripustne_vstupy[0]:
@@ -79,22 +84,22 @@ elif vybrany_text == pripustne_vstupy[1]:
 elif vybrany_text == pripustne_vstupy[2]:
     TEXT_FINAL_LIST = TEXTS[2].split( )
 else:
-    print(50*"=")
+    print(caradvoj_dlouha)
     print(f"'{vybrany_text}' input is incorrect, the program will be terminated.")
-    print(50*"=")
+    print(caradvoj_dlouha)
     exit()
 # print(TEXT_FINAL_LIST)    # Kontrolní print
 
 # Záhlaví vyhodnocení:
-print(50*"=")
+print(caradvoj_dlouha)
 print(f"TEXT ANALYSIS OF TEXT NR. {vybrany_text}:")
-print(50*"=")
+print(caradvoj_dlouha)
 
 # Analyzované parametry textu:
 # Počet slov textu:
 pocet_slov = len(TEXT_FINAL_LIST)
 print("Number of words in text:", pocet_slov)
-print(50*"-")
+print(carajedno_dlouha)
 
 # Počet slov začínajících velkým písmenem:
 pocet_slov_Title = 0
@@ -103,7 +108,7 @@ for slovo in TEXT_FINAL_LIST:
         pocet_slov_Title += 1
         # print(slovo)      # Kontrolní print selektovaných slov
 print("Number of titlecase words:", pocet_slov_Title)
-print(50*"-")
+print(carajedno_dlouha)
 
 # Počet slov psaných velkými písmeny:
 pocet_slov_UPPER = 0
@@ -112,7 +117,7 @@ for slovo in TEXT_FINAL_LIST:
         pocet_slov_UPPER += 1
         # print(slovo)      # Kontrolní print selektovaných slov
 print("Number of uppercase words:", pocet_slov_UPPER)
-print(50*"-")
+print(carajedno_dlouha)
 
 # Počet slov psaných malými písmeny:
 pocet_slov_lower = 0
@@ -121,7 +126,7 @@ for slovo in TEXT_FINAL_LIST:
         pocet_slov_lower += 1
         # print(slovo)      # Kontrolní print selektovaných slov
 print("Number of lowercase words:", pocet_slov_lower)       
-print(50*"-")
+print(carajedno_dlouha)
 
 # Počet čísel:
 pocet_cisel = 0
@@ -130,7 +135,7 @@ for slovo in TEXT_FINAL_LIST:
         pocet_cisel += 1
         # print(slovo)      # Kontrolní print selektovaných čísel
 print("Number of numeric strings:", pocet_cisel)      
-print(50*"-")
+print(carajedno_dlouha)
 
 # Součet všech čísel:
 soucet_cisel = 0
@@ -140,7 +145,7 @@ for slovo in TEXT_FINAL_LIST:
         soucet_cisel += slovo
         # print(slovo)      # Kontrolní print selektovaných čísel
 print("The sum of all the numbers:", soucet_cisel)        
-print(50*"-")
+print(carajedno_dlouha)
 
 # Četnost délek slov:
 # Převod listu se slovy vybraného textu na list s délkou
@@ -164,6 +169,6 @@ for cislo in vycet_delek_slov:
     cetnost = delky_slov.count(cislo)
     print(f"| {cislo}\t| {cetnost * '*': <20}\t| {cetnost}\t|")
 print("+---------------------------------------+")
-print(50*"=")
+print(caradvoj_dlouha)
 
 
